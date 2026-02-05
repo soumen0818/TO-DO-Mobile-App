132adcf1d9543ae39e63bf5c68f1b71645ce4664
Soumen Das <sdas721444@gmail.com>
2026-02-06 03:10:00 +0530

    feat: restructure RootLayout to include SettingsProvider and GestureHandlerRootView

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { AuthProvider } from "@/contexts/AuthContext";
+import { SettingsProvider } from "@/contexts/SettingsContext";
 import { ThemeProvider } from "@/hooks/useTheme";
+// Import to ensure notification handler is set at app start
+import "@/utils/notificationUtils";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { useFonts } from "expo-font";
 import { Slot } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // Prevent splash screen from auto-hiding
 SplashScreen.preventAutoHideAsync();
@@ -29,12 +33,16 @@ export default function RootLayout() {
   }
 
   return (
-    <AuthProvider>
-      <ConvexProvider client={convex}>
-        <ThemeProvider>
-          <Slot />
-        </ThemeProvider>
-      </ConvexProvider>
-    </AuthProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AuthProvider>
+        <ConvexProvider client={convex}>
+          <SettingsProvider>
+            <ThemeProvider>
+              <Slot />
+            </ThemeProvider>
+          </SettingsProvider>
+        </ConvexProvider>
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
